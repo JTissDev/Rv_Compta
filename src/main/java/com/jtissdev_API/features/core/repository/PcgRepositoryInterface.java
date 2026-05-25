27d7934e8f2849e50712e60c38388a22a50ea7f6
J.Tissier <jtissdev@gmail.com>
2026-05-25 15:29:32 +0200

    Integration de pcgRepository
    Interface
    abstract class
    JsonFIle

---
+++ src/main/java/com/jtissdev_API/features/core/repository/PcgRepositoryInterface.java
@@ -0,0 +1,10 @@
+package com.jtissdev_API.features.core.repository;
+
+/**
+ * Represents a PcgRepository DTO.
+ *
+ * @author jtiss
+ * @since 0.1
+ * @version 1.0.0
+ */public interface PcgRepository {
+}
---
+++ src/test/java/com/jtissdev_API/features/core/repository/JsonFilePcpRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.jtissdev_API.features.core.repository;
+
+/**
+ * Represents a JsonFilePcpRepositoryTest DTO.
+ *
+ * @author jtiss
+ * @since 0.1
+ * @version 1.0.0
+ */public class JsonFilePcpRepositoryTest {
+}
